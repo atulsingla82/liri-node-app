5f21cef35603518bb9349b12fcf634b6a5092184
atulsingla82 <Lucky@Atuls-MacBook-Pro.local>
2017-04-16 08:52:17 -0400

    added txt file

---
+++ keys.js
@@ -0,0 +1,12 @@
+console.log('this is loaded');
+
+exports.twitterKeys = {
+  consumer_key: '	nlTF0x2yya5cjubojCluyJPAU',
+  consumer_secret: 'bdeezQdgvGNTY2bXgaxg6fXYReI6V0uUcU1YuU3fBoI2uCrkVF',
+  access_token_key: '853418532574564352-nmTnSHXzUDWWSw4bMzAz5lg7U4R8R7s',
+  access_token_secret: 'dNNvT9dlFDB8SaNXoPWS6aOzwxdqFvfJjiEz1FirP9q98',
+}
+
+
+
+
