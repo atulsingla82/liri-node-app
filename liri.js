3396fa91f5cae9bf4e4879d3318a45ce17673479
atulsingla82 <Lucky@Atuls-MacBook-Pro.local>
2017-04-16 10:14:13 -0400

    test commit

---
+++ liri.js
@@ -0,0 +1,14 @@
+
+//Data from keys.js to store twitter access keys
+
+var keys = require('./keys.js');
+
+
+// Set up other required variables
+
+var fs = require("fs");
+var twitter = require("twitter");
+var spotify = require("spotify");
+var request = require("request");
+
+
